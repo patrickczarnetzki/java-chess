9ed28613f41d688c5c344cdfcedf933059d33318
Patrick Czarnetzki <info@patrick-czarnetzki.de>
2021-02-17 13:25:32 +0100

    "#43 Topmenu layout implemented"
    "Layout of topmenu implemented. I created a new class named MenuButton
    to create a general style for all the buttons in topmenu. Added four of
    them into the flow layout of topmenu and changed the background color of
    topmenu into a dark color. None of these buttons is starting a function
    this commit was just for styling and layouting."

--- java-chess/src/gui/menu/Topmenu.java
+++ java-chess/src/gui/menu/Topmenu.java
@@ -1,15 +1,41 @@
 package gui.menu;
 
+import java.awt.Color;
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import javax.swing.JPanel;
-
 import gui.Mainframe;
 
 public class Topmenu extends JPanel {
 	private Mainframe mainframe;
+	private MenuButton btnNewGame;
+	private MenuButton btnSaveGame;
+	private MenuButton btnLoadGame;
+	private MenuButton btnOptions;
 	
 	public Topmenu(Mainframe mainframe) {
+		// Initialize Mainframe
 		this.mainframe = mainframe;
-		// TODO Auto-generated method stub
+		// Styling
+		setBackground(Color.decode("#191919"));
+		// Buttons will be stored in flow layout
+		setLayout(new FlowLayout(FlowLayout.LEFT));
+		// Initialize buttons
+		btnNewGame = new MenuButton("New Game");
+		btnSaveGame = new MenuButton("Save Game");
+		btnLoadGame = new MenuButton("Load Game");
+		btnOptions = new MenuButton("Options");
+		// Add action listener to buttons
+		btnNewGame.addActionListener(new TopmenuButtonListener());
+		btnSaveGame.addActionListener(new TopmenuButtonListener());
+		btnLoadGame.addActionListener(new TopmenuButtonListener());
+		btnOptions.addActionListener(new TopmenuButtonListener());
+		// Add buttons to flow layout
+		add(btnNewGame);
+		add(btnSaveGame);
+		add(btnLoadGame);
+		add(btnOptions);
 	}
 	
 	public void startNewGame() {
@@ -26,5 +52,20 @@ public class Topmenu extends JPanel {
 	
 	public void openOptions() {
 		// TODO Auto-generated method stub
+	}
+	
+	private class TopmenuButtonListener implements ActionListener {
+		@Override
+		public void actionPerformed(ActionEvent event) {
+			if(event.getSource().equals(btnNewGame)) {
+				startNewGame();
+			} else if(event.getSource().equals(btnSaveGame)) {
+				saveGame();
+			} else if(event.getSource().equals(btnLoadGame)) {
+				loadGame();
+			} else if(event.getSource().equals(btnOptions)) {
+				openOptions();
+			}
+		}
 	}
 }
