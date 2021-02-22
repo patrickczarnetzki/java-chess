ff1af285e1735251697ca2f33eb3a5e1ca260479
Patrick Czarnetzki <info@patrick-czarnetzki.de>
2021-02-22 01:48:31 +0100

    "#28#29 Bishop movement and attacking implemented"
    "Bishop movement and attacking implemented. Bishops are able now to move
    on the diagonal fields and beat enemies on them."

--- java-chess/src/pieces/Bishop.java
+++ java-chess/src/pieces/Bishop.java
@@ -1,5 +1,8 @@
 package pieces;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.swing.ImageIcon;
 
 import board.Field;
@@ -13,8 +16,129 @@ public class Bishop extends Chesspiece {
 	
 	@Override
 	public boolean isValidMovement(Field endingField) {
-		// TODO Auto-generated method stub
-		return true;
+		// Store IDs of starting and ending field
+		int startingFieldID = getField().getID();
+		int endingFieldID = endingField.getID();
+		// Store starting and ending column for exception handling on column 0 and 7 and movement calculation
+		int startingColumn = getField().getColumn();
+		int endingColumn = endingField.getColumn();
+		// Store starting and ending row for movement calculation
+		int startingRow = getField().getRow();
+		int endingRow = endingField.getRow();
+		// Create a list which stores valid field id's
+		List<Integer> validMovement = new ArrayList<Integer>();
+		// Attacking and Movement is the same
+		// Check movement direction
+		if(startingColumn<endingColumn && startingRow>endingRow) {
+			// Bishop goes to the upper right diagonal
+			// Check for a free route to ending field
+			for(int i=startingFieldID-7; i>=endingFieldID; i=i-7) {
+				// Check if field is occupied
+				if(endingField.getBoard().getFieldByID(i).isOccupied()) {
+					// Check if field is occupied by own color (= no free route)
+					if(endingField.getBoard().getFieldByID(i).getChesspiece().isBlack()==isBlack()) {
+						break;
+					// Field is occupied by opponent color and it is not the ending field
+					} else if(i!=endingFieldID) {
+						break;
+					// Field is occupied by opponent color and it is the ending field
+					} else {
+						validMovement.add(i);
+						break;
+					}
+				} else {
+					// If that field is the ending field store it in ArrayList
+					if(i==endingFieldID) {
+						validMovement.add(i);
+						break;
+					}
+				}
+			}
+		}
+		if(startingColumn>endingColumn && startingRow>endingRow) {
+			// Bishop goes to the upper left diagonal
+			// Check for a free route to ending field
+			for(int i=startingFieldID-9; i>=endingFieldID; i=i-9) {
+				// Check if field is occupied
+				if(endingField.getBoard().getFieldByID(i).isOccupied()) {
+					// Check if field is occupied by own color (= no free route)
+					if(endingField.getBoard().getFieldByID(i).getChesspiece().isBlack()==isBlack()) {
+						break;
+					// Field is occupied by opponent color and it is not the ending field
+					} else if(i!=endingFieldID) {
+						break;
+					// Field is occupied by opponent color and it is the ending field
+					} else {
+						validMovement.add(i);
+						break;
+					}
+				} else {
+					// If that field is the ending field store it in ArrayList
+					if(i==endingFieldID) {
+						validMovement.add(i);
+						break;
+					}
+				}
+			}
+		}
+		if(startingColumn<endingColumn && startingRow<endingRow) {
+			// Bishop goes to the lower right diagonal
+			// Check for a free route to ending field
+			for(int i=startingFieldID+9; i<=endingFieldID; i=i+9) {
+				// Check if field is occupied
+				if(endingField.getBoard().getFieldByID(i).isOccupied()) {
+					// Check if field is occupied by own color (= no free route)
+					if(endingField.getBoard().getFieldByID(i).getChesspiece().isBlack()==isBlack()) {
+						break;
+					// Field is occupied by opponent color and it is not the ending field
+					} else if(i!=endingFieldID) {
+						break;
+					// Field is occupied by opponent and it is the ending field
+					} else {
+						validMovement.add(i);
+						break;
+					}
+				} else {
+					// If that field is the ending field store it in ArrayList
+					if(i==endingFieldID) {
+						validMovement.add(i);
+						break;
+					}
+				}
+			}
+		}
+		if(startingColumn>endingColumn && startingRow<endingRow) {
+			// Bishop goes to the lower left diagonal
+			// Check for a free route to ending field
+			for(int i=startingFieldID+7; i<=endingFieldID; i=i+7) {
+				// Check if field is occupied
+				if(endingField.getBoard().getFieldByID(i).isOccupied()) {
+					// Check if field is occupied by own color (= no free route)
+					if(endingField.getBoard().getFieldByID(i).getChesspiece().isBlack()==isBlack()) {
+						break;
+					// Field is occupied by opponent color and it is not the ending field
+					} else if(i!=endingFieldID) {
+						break;
+					// Field is occupied by opponent color and it is the ending field
+					} else {
+						validMovement.add(i);
+						break;
+					}
+				} else {
+					// If that field is the ending field store it in ArrayList
+					if(i==endingFieldID) {
+						validMovement.add(i);
+						break;
+					}
+				}
+			}
+		}
+		// Check for valid movement
+		for(int i=0; i<validMovement.size(); i++) {
+			if(validMovement.get(i)==endingFieldID) {
+				return true;
+			}
+		}
+		return false;
 	}
-	
 }
